f578d710461fed4083178b26d7fce4bf49eb611b
Aigerim <52836936+kydyrbekova@users.noreply.github.com>
2021-02-03 14:19:27 +0600

    Update index.js
    Add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+  var result;
+  var counter = 0;
+  var arr_str = [];
+  if(n>0){
+    result = n.toString().split("").reverse().join("");
+  }else {
+    result = n.toString().split("");
+    for(var i=1; i<result.length; i++){
+      arr_str[counter]=result[i];
+      counter++;
+    }
+    result = arr_str.reverse().join("");
+    
+  }  
+  return parseInt(result);
 }
